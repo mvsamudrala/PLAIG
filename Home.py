efe91af21b28709132fc7b721b01c6394d42391d
Madhav Samudrala <mvsamudrala@Madhavs-MacBook-Pro.local>
2024-12-02 17:17:15 -0500

    Initial commit

---
+++ Home.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="Multipage App")
+st.markdown("<h1 style='text-align: center;'>Welcome to PLAIG's Documentation Webpage</h1>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; font-size: 24px'>PLAIG is a GNN-based deep learning model for protein-ligand binding affinity "
+            "prediction. This app provides documentation on how to use PLAIG and details how PLAIG generates "
+            "graph representations to predict binding affinity. By clicking on the '>' in the top left and navigating to the side bar, "
+            "you will be able to test PLAIG's binding affinity prediction model by submitting your own protein-ligand "
+            "complex in .pdb and .pdbqt files. You can read the citation listed below for background information on "
+            "PLAIG before navigating through this webpage.</p>", unsafe_allow_html=True)
+st.image("/Users/mvsamudrala/BindingAffinityGNN/PLAIG_GUI/GNN Model Framework.png")
+st.markdown("<p style='text-align: left; font-size: 20px'><b>Citations:</b><br> In pre-publication stage.</p>", unsafe_allow_html=True)
+
+
---
+++ pages/Demo.py
@@ -0,0 +1,46 @@
+import time
+import pickle
+import os
+import warnings
+import streamlit as st
+import PLAIG_Run
+import matplotlib.pyplot as plt
+import networkx as nx
+
+
+current_directory = os.getcwd()
+st.markdown("<h1 style='text-align: center;'>PLAIG Demo</h1>", unsafe_allow_html=True)
+st.markdown('<div style="text-align: center;"><a href="https://github.com/mvsamudrala/PLAIG/tree/main/demo_files" target="_blank">Click here '
+            'to download example files for demo testing.</a>', unsafe_allow_html=True)
+form = st.form(key="Options")
+complex_files = form.file_uploader("Submit protein and ligand files (1. protein.pdb, 2. protein.pdbqt, 3. ligand.pdb, 4. ligand.pdbqt)", accept_multiple_files=True)
+complex_files_paths = []
+count = 1
+if complex_files:
+    if len(complex_files) > 4:
+        st.warning("You can only upload up to 4 files.")
+    else:
+        for file in complex_files:
+            form.write(f"File name: {count}. {file.name}")
+            new_file_path = os.path.join(current_directory, file.name)
+            with open(new_file_path, 'wb') as f:
+                f.write(file.getbuffer())
+            complex_files_paths.append(new_file_path)
+            count += 1
+submitted = form.form_submit_button("Submit Files")
+if submitted:
+    complex_files_paths = [tuple(complex_files_paths[i:i + 4]) for i in range(0, len(complex_files_paths), 4)]
+    prediction, graph, color_cutoff = PLAIG_Run.run_model(complex_files_paths)
+    node_colors = ["lightblue" if node < color_cutoff else "pink" for node in graph.nodes()]
+    plt.figure(figsize=(8, 6), dpi=600)
+    nx.draw(graph, with_labels=True, node_color=node_colors, edge_color="black", font_weight="bold", node_size=500, width=3)
+    caption = "Visualization of protein-ligand graph with ligand atoms in blue and protein atoms in pink"
+    plt.figtext(0.5, -0.10, caption, wrap=True, horizontalalignment='center', fontsize=14, family='sans-serif')
+    st.pyplot(plt)
+    plt.clf()
+    st.markdown(
+        f"<p style='text-align: center; color: red; font-size: 24px'>{prediction[0]}.</p>",
+        unsafe_allow_html=True)
+
+
+
